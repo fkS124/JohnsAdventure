4e8560718b9806a1e94eb678f8f1917ba7c16ff5
ƒkS124 <57041516+fkS124@users.noreply.github.com>
2021-10-02 22:01:20 +0200

    Added sound and upgraded the dummy
    Sound manager is now usable everywhere
    Dummy now do a SFX and animation when getting hit

--- data/scripts/enemies.py
+++ data/scripts/enemies.py
@@ -12,10 +12,12 @@ class Enemy:
 
             self.attackable = True
             self.screen = screen
-
-            self.img = scale(load("data/sprites/dummie_alpha.png", alpha=True), 4)
+            
+            self.sheet = load("data/sprites/dummy.png", alpha=True)
+            self.idle = scale(get_sprite(self.sheet, 0, 0, 34, 48), 4)
+            self.hit_anim = [scale(get_sprite(self.sheet, i * 34, 48, 34, 48), 4) for i in range(4)]
             self.x, self.y = pos
-            self.Rect = self.img.get_rect(topleft=pos)
+            self.Rect = self.idle.get_rect(topleft=pos)
 
             self.hp = hp
             self.MAX_HP = hp
@@ -24,8 +26,15 @@ class Enemy:
 
             self.damages_texts = []
 
+            self.id_anim = 0
+            self.hitted = False
+            self.delay = 0
+
         def deal_damage(self, value:int):
             self.hp -= ceil(value - self.endurance)
+            if self.hitted:
+                self.id_anim = 0
+            self.hitted = True
             # -> crit : dmg_type="crit" -> health ; dmg_type="health" ...
             self.damages_texts.append(DamagePopUp(self.screen, self.Rect, ceil(value-self.endurance), dmg_type="default")) 
             if self.hp <= 0:
@@ -34,7 +43,18 @@ class Enemy:
 
         def update(self, scroll):
             
-            self.screen.blit(self.img, (self.x-scroll[0], self.y-scroll[1]))
+            if not self.hitted:
+                self.screen.blit(self.idle, (self.x-scroll[0], self.y-scroll[1]))
+            else:
+                if pg.time.get_ticks() - self.delay > 100:
+                    if self.id_anim + 1 < len(self.hit_anim):
+                        self.id_anim += 1
+                        self.delay = pg.time.get_ticks()
+                    else:
+                        self.hitted = False
+                        self.id_anim = 0
+                cur_frame = self.hit_anim[self.id_anim]
+                self.screen.blit(cur_frame, (self.x-scroll[0], self.y-scroll[1]))
             pos = self.x-scroll[0], self.y-scroll[1]
 
             for dmg_txt in self.damages_texts:
@@ -42,8 +62,8 @@ class Enemy:
                 if kill == "kill":
                     self.damages_texts.remove(dmg_txt)
 
-            self.Rect = self.img.get_rect(topleft=pos)
+            self.Rect = self.idle.get_rect(topleft=pos)
             
             if self.MAX_HP != self.hp:
-                pg.draw.rect(self.screen, (0, 0, 0), [pos[0], pos[1]-20, self.img.get_width(), 15])
-                pg.draw.rect(self.screen, (255, 0, 0), [pos[0], pos[1]-20, int((self.img.get_width()/self.MAX_HP)*self.hp), 15])
+                pg.draw.rect(self.screen, (0, 0, 0), [pos[0], pos[1]-20, self.idle.get_width(), 15])
+                pg.draw.rect(self.screen, (255, 0, 0), [pos[0], pos[1]-20, int((self.idle.get_width()/self.MAX_HP)*self.hp), 15])
---
+++ data/scripts/sound_manager.py
@@ -0,0 +1,47 @@
+import pygame as pg
+
+
+class SoundManager:
+
+    def __init__(self, sound_only:bool=False, music_only:bool=False) -> None:
+        # in order to simplify the load and optimize things, we will load this class
+        # several times but only part of it, for example, in the player class,
+        # you will load sound_only = True because you don't need an access to the
+        # musics and only in the sounds.
+
+        if not pg.mixer.get_init():
+            pg.mixer.init() 
+
+        self.volume = 0.5
+
+        self.sounds = {
+            "letterSound": pg.mixer.Sound("data/sound/letter_sound.wav")
+        } if not music_only else {}
+
+        self.musics = {
+            "forest_theme": "data/sound/forest_theme_part1.flac",
+            "Select_UI": "data/sound/Select_UI.wav"
+        } if not sound_only else {}
+
+    def play_sound(self, key: str) -> bool: # return False if the sound couldn't be player, True otherwise
+        if key not in self.sounds:
+            print("Error when playing sound : unable to find ", key, "in database.")
+            return False
+        self.sounds[key].play()
+        return True
+
+    def play_music(self, key:str) -> bool: # return False if the music couldn't be played, True otherwise
+        # start playing a song, if a song is already playing, it stops the current song and load the
+        # song asked
+
+        pg.mixer.music.set_volume(self.volume)
+
+        if key not in self.musics:
+            return False
+
+        if pg.mixer.music.get_busy():
+            pg.mixer.music.stop()
+            pg.mixer.music.unload()
+
+        pg.mixer.music.load(self.musics[key])
+        pg.mixer.music.play()
